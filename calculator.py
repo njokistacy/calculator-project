4e29be18858a8ce3e872d14716749573d37f7f0a
njokistacy <stacynjoks@gmail.com>
2023-02-15 11:39:37 +0300

    added calculator functions

--- calculator.py
+++ calculator.py
@@ -1,19 +1,32 @@
 number1=eval(input("Enter the first number:"))
 number2=eval(input("Enter the second number:"))
-operator=input("Enter operator")
+operator=input("Enter operator:")
 
-def add(num1, num2):
-   result= num1 +num2
-   return result
+def add(number1, number2):
+    result= number1+number2
+    print(result)
 
-def subtract(num1, num2):
-    result=num1 -num2
-    return result
+def subtract(number1, number2):
+    result=number1-number2
+    print(result)
 
-def divide(num1, num2):
-    result=num1/num2
-    return result
+def divide(number1, number2):
+    result=number1/number2
+    print(result)
 
-def multiply(num1, num2):
-    result=num1*num2
-    return result
+def multiply(number1, number2):
+    result=number1*number2
+    print(result)
+
+#check operator
+if operator=="+":
+    add(number1, number2)
+elif operator=="-":
+    subtract(number1, number2)
+elif operator=="/":
+    divide(number1, number2)
+elif operator=="X" or operator=="x" or operator=="*":
+    multiply(number1, number2)
+else:
+    print("Invalid operational sign")
+    
